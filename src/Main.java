7d1ce806cb8b6898478bb2026f9cffe65b4ff011
mahek522 <maheksultana152@gmail.com>
2026-02-12 15:13:56 +0530

    Coded to Render OOPS as Banner using methods for each letter

--- src/Main.java
+++ src/Main.java
@@ -57,6 +57,16 @@
  * @author Developer
  * @version 5.0
  */
+ /**
+ * OOPSBannerApp UC6 – OOPS Banner Application (Use Case 6)
+ *
+ * This use case extends UC5 by implementing a modular approach to generate each
+ * letter's pattern through dedicated methods. This enhances code reusability and
+ * maintainability by separating pattern generation logic from the main display logic.
+ *
+ * @author Developer
+ * @version 6.0
+ */
 public class Main
 {
     public static void main(String[] args){
@@ -127,5 +137,53 @@ public class Main
             System.out.println(line);
         }
 
+        //UC6
+        // Extend the User Story 5 to display the OOPS banner using a modular approach
+        //Declare String Arrays to hold the Patterns for each letter
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        //Use the loop to Assemble each line of the banner to create
+        // the visual effect for the message 'OOPS'
+        for(int i=0; i<oPattern.length; i++){
+            System.out.println(oPattern[i]+ " "+oPattern[i]+" "+ pPattern[i]+" "+sPattern[i]);
+        }
+    }
+
+    // Method to generate the pattern for the letter 'O'
+    public static String[] getOPattern(){
+        return new String[] {
+                "   ***   ",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "   ***   "
+        };
+    }
+    // Method to generate the pattern for the letter 'P'
+    public static  String[] getPPattern(){
+        return new String[] {
+                "******  ",
+                "**    **",
+                "**    **",
+                "******",
+                "**",
+                "**",
+                "**",
+        };
+    }
+    // Method to generate the pattern for the letter 'S'
+    public static String[] getSPattern(){
+        return new String[] {
+                "   *****",
+                " **   ",
+                "**   ",
+                "    ***",
+                "           ** ",
+                "             **",
+                "       *****   "
+        };
     }
 }
